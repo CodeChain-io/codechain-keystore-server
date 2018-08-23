8588dafcabb985d328f351019f1994ed5d7836e5
Kwang Yul Seo <kseo@kodebox.io>
2018-08-23 18:46:26 +0900

    Close the context after each test

--- src/__test__/pign.spec.ts
+++ src/__test__/pign.spec.ts
@@ -1,11 +1,19 @@
 import { Application } from "express";
 import { createApp } from "../app";
+import { Context, closeContext } from "../context";
 import * as request from "supertest";
 
 let app: Application;
+let context: Context;
 
 beforeEach(async () => {
-    app = (await createApp())[0];
+    const res = await createApp();
+    app = res[0];
+    context = res[1];
+});
+
+afterEach(async () => {
+    await closeContext(context);
 });
 
 test("ping", async () => {
