57d84ec67dcdfab0302aa2e4d8f8e77bceab4a16
Joonmo Yang <jmyang@kodebox.io>
2019-04-30 04:15:02 +0900

    Implement routes with db backend

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,97 +1,194 @@
+import * as storage from "codechain-keystore/lib/logic/storage";
+import { KeyType as KeyTypeId } from "codechain-keystore/lib/model/keytypes";
+import {
+    generatePrivateKey,
+    getAccountIdFromPublic,
+    getPublicFromPrivate,
+    signEcdsa
+} from "codechain-primitives";
 import * as express from "express";
 import { Context } from "../context";
+import KeyModel from "../models/key";
 
 type KeyType = "asset" | "platform";
+
+function findKey(
+    type: KeyType,
+    address: string
+): Promise<KeyModel | undefined> {
+    return KeyModel.query().findById([type, address]) as any;
+}
 
 export function createRouter(context: Context) {
     const router = express.Router();
 
     router.get("/keys", async (req, res) => {
         const keyType: KeyType = req.body.keyType;
-        const keys = await context.cckey[keyType].getKeys();
-
         res.json({
             success: true,
-            result: keys
+            result: await KeyModel.query().where({ type: keyType })
         });
     });
 
     router.get("/keys/:key/publicKey", async (req, res) => {
         const { key } = req.params;
         const keyType: KeyType = req.body.keyType;
-        const publicKey = await context.cckey[keyType].getPublicKey({ key });
 
-        res.json({
-            success: true,
-            result: publicKey
-        });
+        const row = await findKey(keyType, key);
+        if (row == null) {
+            res.json({
+                success: false,
+                error: "Not found"
+            });
+        } else {
+            const priv = await storage.decode(row.toJSON(), "");
+            const pub = getPublicFromPrivate(priv);
+            res.json({
+                success: true,
+                result: pub
+            });
+        }
     });
 
     router.get("/keys/:key/rawKey", async (req, res) => {
-        try {
-            const { key } = req.params;
-            const { passphrase } = req.body;
-            const keyType: KeyType = req.body.keyType;
-            const rawKey = await context.cckey[keyType].exportRawKey({
-                key,
-                passphrase
-            });
+        const { key } = req.params;
+        const { passphrase } = req.body;
+        const keyType: KeyType = req.body.keyType;
 
-            res.json({
-                success: true,
-                result: rawKey
-            });
-        } catch (e) {
+        const row = await findKey(keyType, key);
+        if (row == null) {
             res.json({
                 success: false,
-                error: e
+                error: "Not found"
             });
+        } else {
+            try {
+                const priv = await storage.decode(row.toJSON(), passphrase);
+                res.json({
+                    success: true,
+                    result: priv
+                });
+            } catch (e) {
+                res.json({
+                    success: false,
+                    error: "Invalid passphrase"
+                });
+            }
         }
     });
 
     router.post("/keys", async (req, res) => {
         const { passphrase } = req.body;
         const keyType: KeyType = req.body.keyType;
-        const key = await context.cckey[keyType].createKey({
-            passphrase
-        });
-        res.json({
-            success: true,
-            result: key
-        });
+
+        while (true) {
+            const priv = generatePrivateKey();
+            const pub = getPublicFromPrivate(priv);
+            const address = getAccountIdFromPublic(pub);
+
+            // Check duplication
+            if ((await findKey(keyType, address)) != null) {
+                continue;
+            }
+
+            let typeId: KeyTypeId | undefined;
+            switch (keyType) {
+                case "asset": {
+                    typeId = KeyTypeId.Asset;
+                    break;
+                }
+                case "platform": {
+                    typeId = KeyTypeId.Platform;
+                    break;
+                }
+            }
+
+            const secret = await storage.encode(
+                priv,
+                typeId!,
+                passphrase,
+                JSON.stringify({})
+            );
+            await KeyModel.query().insert({
+                type: keyType,
+                address: secret.address,
+                version: secret.version,
+
+                kdf: secret.crypto.kdf,
+                kdfparams: secret.crypto.kdfparams,
+                mac: secret.crypto.mac,
+
+                cipher: secret.crypto.cipher,
+                cipherparams: secret.crypto.cipherparams,
+                ciphertext: secret.crypto.ciphertext,
+
+                meta: secret.meta
+            });
+
+            res.json({
+                success: true,
+                result: secret.address
+            });
+            return;
+        }
     });
 
     router.delete("/keys/:key", async (req, res) => {
         const { key } = req.params;
         const keyType: KeyType = req.body.keyType;
-        const result = await context.cckey[keyType].deleteKey({
-            key
-        });
-        res.json({
-            success: true,
-            result
-        });
+
+        const row = await findKey(keyType, key);
+
+        if (row == null) {
+            res.json({
+                success: false,
+                error: "Not found"
+            });
+        } else {
+            try {
+                // Check if the passphrase is correct
+                await storage.decode(row.toJSON(), "");
+                await KeyModel.query()
+                    .del()
+                    .findById([keyType, key]);
+                res.json({
+                    success: true,
+                    result: true
+                });
+            } catch (e) {
+                res.json({
+                    success: true,
+                    result: false
+                });
+            }
+        }
     });
 
     router.post("/keys/:key/sign", async (req, res) => {
-        try {
-            const { key } = req.params;
-            const { message, passphrase = "" } = req.body;
-            const keyType: KeyType = req.body.keyType;
-            const result = await context.cckey[keyType].sign({
-                key,
-                passphrase,
-                message
-            });
-            res.json({
-                success: true,
-                result
-            });
-        } catch (e) {
+        const { key } = req.params;
+        const { message, passphrase = "" } = req.body;
+        const keyType: KeyType = req.body.keyType;
+
+        const row = await findKey(keyType, key);
+
+        if (row == null) {
             res.json({
                 success: false,
-                error: e
+                error: "Not found"
             });
+        } else {
+            try {
+                const priv = await storage.decode(row.toJSON(), passphrase);
+                res.json({
+                    success: true,
+                    result: signEcdsa(message, priv)
+                });
+            } catch (e) {
+                res.json({
+                    success: false,
+                    error: e
+                });
+            }
         }
     });
 
